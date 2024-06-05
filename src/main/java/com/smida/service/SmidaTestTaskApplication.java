58a9b30e8eaf52c25b00f3a8ff51e19efe9d1819
Svitlana-Marchenko <sveta25022004@gmail.com>
2024-06-05 18:31:13 +0300

    first commit

---
+++ src/main/java/com/smida/service/SmidaTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.smida.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmidaTestTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmidaTestTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/smida/service/SmidaTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smida.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmidaTestTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
